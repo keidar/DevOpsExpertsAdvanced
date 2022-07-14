19b3135dc7e0a339b8bf52e89ec86979691b544c
Keidar Ben Yaakov <kyaakov@datto.com>
2022-07-14 15:52:12 +0300

    update cd pipeline

--- k8s-project/pipelines/cd.pipeline.groovy
+++ k8s-project/pipelines/cd.pipeline.groovy
@@ -8,8 +8,8 @@ pipeline {
         kind: Pod
         spec:
           containers:
-          - name: ubuntu
-            image: ubuntu:20.04 
+          - name: helm
+            image: alpine/k8s:1.22.10
             command:
             - cat
             tty: true
@@ -20,17 +20,15 @@ pipeline {
     	stage('CD Pipeline') {
     		steps {
         			git branch: 'master', changelog: false, poll: false, url: 'https://github.com/keidar/DevOpsExpertsAdvanced.git'
-          script{
-              sh 'apt-get install sudo'
-              sh 'sudo apt-get install curl gpg'
-              sh 'curl https://baltocdn.com/helm/signing.asc | gpg --dearmor |  tee /usr/share/keyrings/helm.gpg > /dev/null'
-              sh 'sudo apt-get install apt-transport-https --yes'
-              sh 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list'
-              sh 'apt-get update'
-              sh 'apt-get install helm'
-              sh " helm upgrade --install rabbit ./k8s-project/helm_charts/rabbitmq"
-              sh " helm upgrade --install rabbit ./k8s-project/helm_charts/consumer"
-              sh " helm upgrade --install rabbit ./k8s-project/helm_charts/producer"
+          container('helm'){
+            script{
+              sh 'ls -ltr '
+              sh 'helm version'
+              dir("k8s-project/helm_charts"){
+                  sh " helm upgrade --install producer producer"
+                  sh " helm upgrade --install consumer consumer"
+              }           
+            }
           }
     	}
     }  
